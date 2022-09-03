1d69900a8d765adc8840888e4c46158882b00a6a
andr30z <andrelp1015@gmail.com>
2022-09-03 15:00:35 -0300

    build: change android build sdk versions

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import { registerRootComponent } from 'expo';
  
 import App from './App';
-import 'intl';
-import 'intl/locale-data/jsonp/en';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
