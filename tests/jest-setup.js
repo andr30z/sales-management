0509ea683bcac8bc790bd5ef737a00a2cd0cf297
andr30z <andrelp1015@gmail.com>
2022-09-26 21:30:14 -0300

    chore: remove unused imports

--- tests/jest-setup.js
+++ tests/jest-setup.js
@@ -1,7 +1,3 @@
-// import 'react-native-gesture-handler/jestSetup';
-
-// jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
-
 jest.mock('react-native-reanimated', () => {
   const Reanimated = require('react-native-reanimated/mock');
 
