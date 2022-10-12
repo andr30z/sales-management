88dde78c7a75f070926c8e1ac4a088555584b1c5
andr30z <andrelp1015@gmail.com>
2022-10-12 01:31:12 -0300

    fix: add responsivity for sales list items

--- src/Components/ListingScreenItem/index.tsx
+++ src/Components/ListingScreenItem/index.tsx
@@ -74,10 +74,10 @@ export const ListingScreenItem = <I extends { id: string }>({
             {name}
           </Text>
           <Container
-            flex={null as any}
+            flex={null}
             width="100%"
             flexDirection="row"
-            alignItems="baseline"
+            flexWrap="wrap"
           >
             <Text
               status={resolveTextColor()}
